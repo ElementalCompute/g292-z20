42aaa5642cf654783aa892a75d56d03d874d0973
Aaron Ghani <aaronghani@Aarons-MacBook-Pro.local>
2025-11-05 09:39:53 +0000

    Restart bmc after updates

--- update-bmc.sh
+++ update-bmc.sh
@@ -87,3 +87,4 @@ ipmitool -I open user list "$chan" | awk -v uid="$admin_uid" '$1==uid'
 echo
 echo "Note: Some BMCs apply LAN changes after a BMC reboot. If unreachable on the new IP, you can do:"
 echo "      ipmitool -I open bmc reset cold   # (host stays up)"
+ipmitool -I open bmc reset cold
